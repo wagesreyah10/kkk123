a1f962edaa1f3e2fa28a306f6d3e0a4ac1f16197
alan <168492923+alan349@users.noreply.github.com>
2025-06-13 18:38:34 +0700

    Delete api/env.js

--- api/env.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(request, response) {
-  response.status(200).json({
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
-   
-  });
-}
